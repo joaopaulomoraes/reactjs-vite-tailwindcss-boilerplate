58d8b8bd022b2a87bf318c5b65bf9a457a9e7de9
João Paulo Moraes <joaopaulomoraes@outlook.com>
2021-11-14 12:39:33 -0300

    Change vite refresh render
    - upgrade packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
 import 'tailwindcss/tailwind.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 )
