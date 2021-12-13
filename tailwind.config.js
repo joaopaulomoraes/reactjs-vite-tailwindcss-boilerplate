edeb9d34ca49e3017a164ea387a026eac71b7f56
João Paulo Moraes <joaopaulomoraes@outlook.com>
2021-12-13 00:02:07 -0300

    Update tailwindcss to latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
-  },
-  variants: {
     extend: {}
   },
   plugins: []
