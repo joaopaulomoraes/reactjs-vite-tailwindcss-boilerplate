0fabceb3b7a410295f994e2685163551c6b73879
João Paulo Moraes <joaopaulomoraes@outlook.com>
2023-01-06 15:03:34 -0300

    Improve welcome page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,22 +2,98 @@ import Avatar from 'components/Avatar'
 
 function App() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-screen-xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h2 className="text-base font-semibold uppercase tracking-wide text-blue-600">
-            Welcome to
-          </h2>
-          <p className="my-3 text-4xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            reactjs-vite-tailwindcss-boilerplate
-          </p>
-          <p className="text-xl text-gray-400">Start building for free.</p>
-          <p className="mt-5">
-            <Avatar
-              size="large"
-              src="https://www.gravatar.com/avatar/4405735f6f3129e0286d9d43e7b460d0"
-            />
-          </p>
+    <div className="relative overflow-hidden bg-white">
+      <div className="h-screen sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
+        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+          <div className="sm:max-w-lg">
+            <div className="my-4">
+              <Avatar
+                size="large"
+                src="https://www.gravatar.com/avatar/4405735f6f3129e0286d9d43e7b460d0"
+              />
+            </div>
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Welcome!
+            </h1>
+            <p className="mt-4 text-xl text-gray-500">
+              This is a boilerplate build with React 18, TypeScript, Jest,
+              Testing Library, TailwindCSS 3, Vite, Eslint and Prettier.
+            </p>
+          </div>
+          <div>
+            <div className="my-10">
+              <a
+                href="vscode://"
+                className="inline-block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-center font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2"
+              >
+                Start building for free
+              </a>
+              <div
+                aria-hidden="true"
+                className="pointer-events-none mt-10 md:mt-0 lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
+              >
+                <div className="absolute sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
+                  <div className="flex items-center space-x-6 lg:space-x-8">
+                    <div className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100">
+                        <img
+                          src="https://picsum.photos/600?random=1"
+                          alt=""
+                          className="h-full w-full bg-indigo-100 object-cover object-center"
+                        />
+                      </div>
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://picsum.photos/600?random=2"
+                          alt=""
+                          className="h-full w-full bg-indigo-100 object-cover object-center"
+                        />
+                      </div>
+                    </div>
+                    <div className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://picsum.photos/600?random=3"
+                          alt=""
+                          className="h-full w-full bg-indigo-100 object-cover object-center"
+                        />
+                      </div>
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://picsum.photos/600?random=4"
+                          alt=""
+                          className="h-full w-full bg-indigo-100 object-cover object-center"
+                        />
+                      </div>
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://picsum.photos/600?random=5"
+                          alt=""
+                          className="h-full w-full bg-indigo-100 object-cover object-center"
+                        />
+                      </div>
+                    </div>
+                    <div className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://picsum.photos/600?random=6"
+                          alt=""
+                          className="h-full w-full bg-indigo-100 object-cover object-center"
+                        />
+                      </div>
+                      <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        <img
+                          src="https://picsum.photos/600?random=7"
+                          alt=""
+                          className="h-full w-full bg-indigo-100 object-cover object-center"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
