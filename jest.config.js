61d0f249b9cfc4108e99eb48fc48e326dbd7d73e
João Paulo Moraes <joaopaulomoraes@outlook.com>
2023-02-03 17:05:43 -0300

    Remove jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
-}
