ceaea447f7f4ef7ed9cc125d0df7a2d44f9a228b
João Paulo Moraes <joaopaulomoraes@outlook.com>
2023-09-24 09:53:52 -0300

    Split public & assets

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import Avatar from 'components/Avatar'
+import logo from 'assets/logo.svg'
 
 const randoms = [
   [1, 2],
@@ -13,10 +14,7 @@ function App() {
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
           <div className="sm:max-w-lg">
             <div className="my-4">
-              <Avatar
-                size="large"
-                src="https://www.gravatar.com/avatar/4405735f6f3129e0286d9d43e7b460d0"
-              />
+              <Avatar size="large" src={logo} />
             </div>
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
               Welcome!
