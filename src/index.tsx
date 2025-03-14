ad690b7b35d3ddcb5bc21e0fa8cfdb52534081a1
Jhon Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2025-03-13 19:28:05 -0500

    rearrange css import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import App from 'components/App'
 import './index.css'
+import App from 'components/App'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
